ccfcd7ab865f586ea8c1af9e0b98c9514afdf51d
VanshSharmaSDE <vansh4664@gmail.com>
2025-09-07 19:18:37 +0530

    Add axios import to server.js for API requests

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require('./config/database');
+const axios = require('axios');
 
 // Load environment variables
 dotenv.config();
